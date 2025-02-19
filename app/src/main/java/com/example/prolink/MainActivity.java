598fef32026b28b9533255b979596645c9142d70
Murilo Pereira <murilopereiranunesrosa@gmail.com>
2025-02-19 13:55:22 -0300

    "Página de Login Criada"

--- app/src/main/java/com/example/prolink/MainActivity.java
+++ app/src/main/java/com/example/prolink/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.prolink;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private TextView mTxtAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+        mTxtAccount = findViewById(R.id.txt_account);
+
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEditEmail.getText().toString();
+                String password = mEditPassword.getText().toString(); // Pegando do campo correto
+
+                Log.i("Teste", "Email: " + email);
+                Log.i("Teste", "Password: " + password);
+            }
+        });
+        mTxtAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Cadastro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
